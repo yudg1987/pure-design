7e4808996a921f82ccbda0cf9aafbfd0cdcc7d53
dgyu <dgyu@iflytek.com>
2022-04-23 01:58:10 +0800

    改变上下文

--- vue/src/utils/request.js
+++ vue/src/utils/request.js
@@ -3,7 +3,7 @@ import router from "@/router";
 import {serverIp} from "../../public/config";
 
 const request = axios.create({
-    baseURL: `http://${serverIp}:9091`,
+    baseURL: `http://${serverIp}:9091/springboot`,
     timeout: 30000
 })
 
